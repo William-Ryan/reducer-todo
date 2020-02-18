1a1a11691cbb19b2351c6ff3d21b418e47b71652
William Ryan <billy.ryan.rom@gmail.com>
2020-02-18 03:01:34 -0500

    Add State Functions to App.js

--- project/src/App.js
+++ project/src/App.js
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 
+const items = [
+  {
+    task: 'Do Laundry',
+    id: 231,
+    completed: false
+  },
+  {
+      task: 'Wash Dishes',
+      id: 232,
+      completed: false 
+  }
+];
+
+
 function App() {
+
+  const [todoList, setTodoList] = useState(items);
+
+  const toggleItem = (clickedId) => {
+    const newTodos = todoList.map(todo => {
+      if (todo.id === clickedId) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      } else {
+        return todo;
+      }
+    })
+    
+    setTodoList({
+      todoList: newTodos
+    })
+  }
+
+  const addNewTodo = itemText => {
+    const newTodo = {
+      task: itemText,
+      id: Date.now(),
+      purchased: false
+    };
+    setTodoList({
+      todoList: [...todoList, newTodo]
+    });
+  };
+
+  const itemDelete = () => {
+    const resetList = todoList.filter(items => !items.completed)
+
+    setTodoList({
+      todoList: resetList
+    })
+  }
+
   return (
     <div className="App">
     
